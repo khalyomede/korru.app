8df21b0e63a9b72dfa5e8e09caa89c54abcd6b63
khalyomede <khalyomede@gmail.com>
2026-01-19 00:03:29 +0100

    add improve loading time

--- vite.config.ts
+++ vite.config.ts
@@ -2,14 +2,26 @@ import { defineConfig } from 'vite';
 import solidPlugin from 'vite-plugin-solid';
 import devtools from 'solid-devtools/vite';
 import tailwindcss from '@tailwindcss/vite'
-
+import { createHtmlPlugin } from 'vite-plugin-html';
 
 export default defineConfig({
-    plugins: [devtools(), solidPlugin(), tailwindcss()],
+    plugins: [
+        devtools(),
+        solidPlugin(),
+        tailwindcss(),
+        createHtmlPlugin({
+            minify: true, // enables minification in production
+        }),
+    ],
     server: {
         port: 3000,
     },
     build: {
         target: 'esnext',
+        minify: 'terser', // or 'esbuild' (but terser gives more control)
+        terserOptions: {
+            compress: true,
+            mangle: true,
+        },
     },
 });
