a71b1a030d16df2f14f4d07c54a0a99ac789184f
khalyomede <khalyomede@gmail.com>
2026-02-14 09:14:11 +0100

    add apps complements to fix or add missing informations in apps manifest

--- src/hooks/useStore.ts
+++ src/hooks/useStore.ts
@@ -1,12 +1,22 @@
 import { createStore, unwrap } from "solid-js/store";
 import Store from "../interfaces/Store";
 import apps from "../data/apps";
+import complements from "../data/complements";
+
+/**
+ * We use "complements" to fix mistakes or missing informations in
+ * apps manifests, like broken icons, missing categories, ...
+ */
+const completedApps = apps.map(app => ({
+    ...app,
+    ...complements.find(complement => complement.id === app.id) || {},
+}));
 
 const localData = window.localStorage.getItem("store");
 const jsonLocalData = localData === null ? { search: "" } : JSON.parse(localData);
 const data: Store = {
     search: jsonLocalData.search ?? "",
-    apps: apps,
+    apps: completedApps,
 };
 
 const useStore = () => {
