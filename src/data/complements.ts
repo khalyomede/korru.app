0887e0228fbea9d9a4a049aabd228ebb34532c26
khalyomede <khalyomede@gmail.com>
2026-03-21 21:26:51 +0100

    add some missing categories to apps

--- src/data/complements.ts
+++ src/data/complements.ts
@@ -6,12 +6,53 @@ import App from "../interfaces/App";
  */
 const complements: Array<Partial<App>> = [
     {
+        id: 1,
+        name: "Spotify",
+        categories: [
+            "music",
+            "entertainment",
+            "multimedia",
+        ],
+    },
+    {
+        id: 5,
+        name: "Youtube",
+        categories: [
+            "entertainment",
+            "multimedia",
+            "education",
+            "news",
+        ]
+    },
+    {
+        id: 7,
+        name: "TheFork",
+        description: "Discover and book the best restaurant",
+        categories: [
+            "food",
+        ],
+    },
+    {
+        id: 8,
+        name: "Google Translate",
+        categories: [
+            "education",
+            "utilities",
+        ],
+    },
+    {
         id: 165,
         name: "Microsoft Bing Webmaster Tools",
         icon: {
             url: "https://www.bing.com/webmasters/Content/PWA/logo/bing-logo-192.png",
             maskable: true
         },
+        categories: [
+            "business",
+            "productivity",
+            "developer",
+            "developer tools",
+        ],
     },
     {
         id: 179,
@@ -20,16 +61,6 @@ const complements: Array<Partial<App>> = [
             url: "https://www.nanocell-csv.com/favicon-96x96.png",
             maskable: false,
         },
-        screenshots: [
-            {
-                url: "https://www.nanocell-csv.com/web/img/screenshot_light.png",
-                name: "App spreadsheet in light mode"
-            },
-            {
-                url: "https://www.nanocell-csv.com/web/img/screenshot_dark.png",
-                name: "App spreadsheet in dark mode"
-            }
-        ]
     },
     {
         id: 198,
@@ -38,10 +69,18 @@ const complements: Array<Partial<App>> = [
             url: "https://bsehovac.github.io/the-cube/assets/icons/android-chrome-192x192.png",
             maskable: false,
         },
+        categories: [
+            "games",
+        ],
     },
     {
         id: 204,
         name: "Slingshot",
+        categories: [
+            "finance",
+            "productivity",
+            "lifestyle",
+        ],
     },
     {
         id: 205,
@@ -51,6 +90,10 @@ const complements: Array<Partial<App>> = [
             url: "https://www.redditstatic.com/shreddit/assets/favicon/192x192.png",
             maskable: false,
         },
+        "categories": [
+            "news",
+            "entertainment",
+        ],
     }
 ];
 
