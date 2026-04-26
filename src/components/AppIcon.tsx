816634dbcc93ab0111ecc3bb9412704ac0bf70cc
khalyomede <khalyomede@gmail.com>
2026-04-26 23:56:46 +0200

    add retry loading icons when browser is back online

--- src/components/AppIcon.tsx
+++ src/components/AppIcon.tsx
@@ -1,8 +1,70 @@
 // src/components/AppIconPreview.tsx
-import { Component } from 'solid-js';
+import { Component, createSignal, onMount } from 'solid-js';
 import AppIconProperties from './properties/AppIconProperties';
 
 const AppIcon: Component<AppIconProperties> = (props) => {
+    const [broken, setBroken] = createSignal(false);
+    const [online, setOnline] = createSignal(true);
+    let imageElement: HTMLImageElement | undefined;
+
+    window.addEventListener("online", () => {
+        setOnline(true);
+
+        if (imageIsWithinViewport()) {
+            retryLoadingIconImage();
+        }
+    });
+
+    window.addEventListener("offline", () => setOnline(false));
+
+    window.addEventListener("scroll", () => {
+        if (imageIsWithinViewport()) {
+            retryLoadingIconImage();
+        }
+    });
+
+    onMount(() => {
+        retryLoadingIconImageWhenCrossingViewport();
+    });
+
+    const retryLoadingIconImageWhenCrossingViewport = () => {
+        if (imageElement instanceof HTMLImageElement) {
+            const observer = new IntersectionObserver(([entry]) => {
+                if (entry.isIntersecting && entry.target instanceof HTMLImageElement) {
+                    retryLoadingIconImage();
+                }
+            }, {
+                root: document.querySelector('body'),
+            });
+
+            observer.observe(imageElement);
+        }
+    };
+
+    const retryLoadingIconImage = () => {
+        if (imageElement instanceof HTMLImageElement && broken() && online()) {
+            imageElement.setAttribute("src", imageElement.src);
+            setBroken(false);
+        }
+    };
+    /**
+     * @see https://stackoverflow.com/a/7557433/3753055
+     */
+    const imageIsWithinViewport = () => {
+        if (!(imageElement instanceof HTMLImageElement)) {
+            return false;
+        }
+
+        const elementBounding = imageElement.getBoundingClientRect();
+
+        return (
+            elementBounding.top >= 0 &&
+            elementBounding.left >= 0 &&
+            elementBounding.bottom <= (window.innerHeight || document.documentElement.clientHeight) && /* or $(window).height() */
+            elementBounding.right <= (window.innerWidth || document.documentElement.clientWidth) /* or $(window).width() */
+        );
+    };
+
     return (
         <div
             classList={{
@@ -20,10 +82,12 @@ const AppIcon: Component<AppIconProperties> = (props) => {
             }}
         >
             <img
+                ref={imageElement}
                 src={props.src.trim()} // Added .trim() to fix whitespace in URLs
                 alt={props.alt}
                 class="w-16 h-16 object-cover bg-stone-50 dark:bg-stone-700"
                 loading="lazy"
+                onError={() => setBroken(true)}
             />
         </div>
     );
