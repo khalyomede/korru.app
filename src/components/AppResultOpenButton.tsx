41982b1c66bed0d32a689ef4dede22ef36a69355
khalyomede <khalyomede@gmail.com>
2026-04-26 14:20:34 +0200

    add emphasize open app button

--- src/components/AppResultOpenButton.tsx
+++ src/components/AppResultOpenButton.tsx
@@ -5,12 +5,17 @@ import { vibrate } from "../utilities";
 const AppResultOpenButton: Component<AppResultOpenButtonProperties> = (properties) => {
     return (
         <a href={properties.url} onclick={() => vibrate(60)} target="_blank" rel="noopener noreferrer" class="
-                px-4 py-2 border border-stone-300 text-stone-500 font-medium text-sm rounded-lg tracking-widest
+                px-4 py-2
+                border border-stone-300 rounded-lg
+                dark:border-stone-600
                 hover:bg-stone-100 hover:border-stone-500 hover:text-stone-900
                 focus:outline-none
-                focus:border-stone-700 focus:text-stone-700 focus:bg-stone-200
-                focus:dark:border-stone-400 focus:dark:text-stone-300 focus:dark:bg-stone-700
-                dark:border-stone-600 dark:text-stone-400
+                bg-stone-400
+                text-stone-100 text-sm tracking-widest font-medium
+                dark:text-stone-800
+                focus:dark:text-stone-100
+                focus:border-stone-700 focus:bg-stone-900
+                focus:dark:border-stone-400  focus:dark:bg-stone-700
                 dark:hover:bg-stone-700 dark:hover:border-stone-500 dark:hover:text-white
                 transition-all duration-200 ease-in-out
                 whitespace-nowrap
