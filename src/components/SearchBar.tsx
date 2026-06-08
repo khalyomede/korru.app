f2ac075ed5c23e9fb7188e5c3d06d1fc8b5572af
khalyomede <khalyomede@gmail.com>
2026-06-08 19:49:49 +0200

    fixed search icon not focusing the input

--- src/components/SearchBar.tsx
+++ src/components/SearchBar.tsx
@@ -3,9 +3,11 @@ import { Component, onMount } from 'solid-js';
 import SearchBarProperties from './properties/SearchBarProperties';
 import { useLocation } from '@solidjs/router';
 import LocationState from '../interfaces/LocationState';
+import { Search } from 'lucide-solid';
 
 const SearchBar: Component<SearchBarProperties> = (properties) => {
     let element: HTMLInputElement | undefined;
+    let input: HTMLInputElement | undefined;
 
     onMount(() => {
         const { state } = useLocation<LocationState>();
@@ -16,11 +18,19 @@ const SearchBar: Component<SearchBarProperties> = (properties) => {
         }
     });
 
+    const onClick = () => {
+        input?.focus();
+    }
+
     return (
-        <div class="w-full">
+        <div class="w-full flex items-center gap-2 relative" ref={element} id={properties.id}>
+            <Search
+                size="21px"
+                class="ml-4 text-stone-400 absolute cursor-pointer"
+                onClick={onClick}
+            />
             <input
-                ref={element}
-                id={properties.id}
+                ref={input}
                 onFocus={properties.onFocus}
                 onInput={properties.onInput}
                 type="search"
@@ -28,7 +38,7 @@ const SearchBar: Component<SearchBarProperties> = (properties) => {
                 autocomplete="off"
                 value={properties.value}
                 placeholder={properties.placeholder}
-                class="w-full px-6 py-3 rounded-2xl border border-stone-300 tracking-wider
+                class="w-full px-6 pl-12 py-3 rounded-2xl border border-stone-300 tracking-wider
                     focus:outline-none focus:dark:border-stone-300
                     focus:border-stone-700
                     text-stone-900 placeholder-stone-400 place
